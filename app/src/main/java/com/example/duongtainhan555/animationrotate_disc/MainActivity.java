93f2e8ea6a04a6df08c9b592da975f261ae5aac8
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-08-02 03:31:08 +0700

    ObjectAnimator

--- app/src/main/java/com/example/duongtainhan555/animationrotate_disc/MainActivity.java
+++ app/src/main/java/com/example/duongtainhan555/animationrotate_disc/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.duongtainhan555.animationrotate_disc;
 
+import android.animation.ObjectAnimator;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -7,32 +8,70 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnPlay, btnPause;
+    private ObjectAnimator anim;
     private ImageView imgDisc;
-    @Override
+    private boolean status = true;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Init
         Init();
-        //EventClick
-        EventClick();
+        CreateAnimation(0,5000,20000);
+        //EventClickPlay
+        btnPlay.setVisibility(View.VISIBLE);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PlayDisc();
+                btnPause.setVisibility(View.VISIBLE);
+                btnPlay.setVisibility(View.INVISIBLE);
+            }
+        });
+        //EventClickPause
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PauseDisc();
+                btnPlay.setVisibility(View.VISIBLE);
+                btnPause.setVisibility(View.INVISIBLE);
+            }
+        });
+
     }
     private void Init()
     {
         imgDisc = findViewById(R.id.imgDisc);
+        btnPlay = findViewById(R.id.btnPlay);
+        btnPause = findViewById(R.id.btnPause);
+
     }
-    private void EventClick()
+    private void CreateAnimation(int from_degrees, int to_degrees, int duration)
     {
-        final Animation animation1 = AnimationUtils.loadAnimation(this,R.anim.rotate_disc);
-        imgDisc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                imgDisc.startAnimation(animation1);
-            }
-        });
+        anim = ObjectAnimator.ofFloat(imgDisc,"rotation",from_degrees,to_degrees);
+        anim.setDuration(duration);
+    }
+    private void PlayDisc()
+    {
+        if(status)
+        {
+            anim.start();
+        }
+        else
+        {
+            anim.resume();
+        }
+    }
+    private void PauseDisc()
+    {
+        anim.pause();
+        status = false;
     }
 }
